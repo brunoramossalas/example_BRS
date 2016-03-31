d42a5fce1a3e0de32dcaed696160cae2d2db777f
Bruno Michael Ramos Salas <brunoramossalas@macbook-pro-de-bruno.local>
2016-03-31 11:57:36 -0500

    Initial Commit

---
+++ Actividad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Actividad
+//
+//  Created by Bruno Michael Ramos Salas on 31/03/16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
